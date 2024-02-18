77d4f78cb3aa876359421f90eefb132e09bc944c
SimaThapa <sima.thapa3333@gmail.com>
2024-02-18 07:24:25 +0545

    chat body

--- client/src/chatpage/chatbar.js
+++ client/src/chatpage/chatbar.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import Profile from '../assets/profile.jfif'
 
 const ChatBar = ({ socket }) => {
   const [users, setUsers] = useState([]);
@@ -8,15 +9,28 @@ const ChatBar = ({ socket }) => {
   }, [socket, users]);
 
   return (
-    <div className="chat__sidebar">
+    <div className="chat__sidebar" >
+      <div>
+         <img src={Profile} alt='profile-pic' height="50px" width="50px" style={{borderRadius:"50%"}}/>
+         <div>
+           <input type="text" placeholder="search uesrs" style={{width:"80%",borderRadius:"20px"}}/>
+         </div>
+      </div>
       <h2>Open Chat</h2>
       <div>
         <h4 className="chat__header">ACTIVE USERS</h4>
+        
         <div className="chat__users">
           {users.map((user) => (
-            <p key={user.socketID}>{user.userName}</p>
-          ))}
+              <p key={user.socketID}>
+                  <button style={{border:"hidden"}}>
+                     {user.userName}
+                  </button>
+              </p>
+            ))}
+         
         </div>
+        
       </div>
     </div>
   );
